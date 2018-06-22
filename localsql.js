d3f1cbbbe6782ad7144aec636e117216783db4c1
HolyMichael <555migalves555@gmail.com>
2018-06-23 00:17:41 +0100

    actually no stupid stuff

--- localsql.js
+++ localsql.js
@@ -4,8 +4,8 @@
 let mysql = require('mysql');
 let con = mysql.createConnection({
   host: "127.0.0.1",
-  user: "migue_server",
-  password: "Wepp801*",
+  user: "root",
+  password: "root",
   database: "somethingbrawl"
 });
 
